5814958619d14de92108bf85a742e267e472064e
iretoms <iretomiwajonathan@gmail.com>
2022-08-13 22:33:26 -0700

    setting up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import AboutUs from "./pages/AboutUs"
+import Community from "./pages/Community"
+import ContactUs from "./pages/ContactUs"
+
 function App() {
   return (
     <div className="App">
-      
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/" element={<Community />} />
+        <Route path="/" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
