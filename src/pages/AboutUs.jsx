c024795c2a5be09bfc6086cabe01676faaf973ab
iretoms <iretomiwajonathan@gmail.com>
2022-08-17 18:09:29 -0700

    first section about page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,11 @@
+import AFirstSection from "../components/AFirstSection";
 import Header from "../components/Header";
 
 const AboutUs = () => {
     return (
         <div>
             <Header/>
+            <AFirstSection/>
         </div>
     );
 };
