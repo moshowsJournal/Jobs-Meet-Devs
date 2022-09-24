ddaa1993dde4cb6ca980aed2f77f5cc59e810862
iretoms <iretomiwajonathan@gmail.com>
2022-09-24 01:18:33 -0700

    works card component

--- src/components/CtaButton.jsx
+++ src/components/CtaButton.jsx
@@ -15,13 +15,11 @@ const Div = styled(motion.div)`
   .ctaBtn {
    display: inline-block;
     background-color: ${(props) => props.theme.primary};
-    padding: 1.4rem 2rem;
+    padding: 1rem 2rem;
     border-radius: 1rem;
     font-size: 1.1rem;
     font-family: "GTWalsheimPro", sans-serif;
     color: #fff;
-    min-width: 10rem;
-    height: 4rem;
   }
 `;
 
---
+++ src/components/WorksCard.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const WorksCard = ({ image, title, description }) => {
+  return (
+    <Card>
+      <div className="icon-container">
+        <img src={image} alt="choose" />
+      </div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </Card>
+  );
+};
+
+const Card = styled(motion.div)`
+  width: 18.3rem;
+  height: 21rem;
+  padding: 3rem;
+  border: 1px solid #e7e9eb;
+  border-radius: 8px;
+  h1 {
+    margin-top: 0.5rem;
+    font-size: 1.3rem;
+    font-weight: 600;
+    font-family: "Open Sans", sans-serif;
+    color: ${(props) => props.theme.primary};
+  }
+
+  p {
+    margin-top: 1rem;
+    font-size: 0.9rem;
+    font-family: "GTWalsheimPro", sans-serif;
+    color: ${(props) => props.theme.text2};
+  }
+`;
+
+export default WorksCard;
